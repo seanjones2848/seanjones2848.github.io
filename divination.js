2e503d1aa3d16cf5940bd3a6052ca4924a9367cc
seanjones2848 <seanjones2848@gmail.com>
2017-11-05 08:41:24 -0800

    added formatting for rng

--- divination.js
+++ divination.js
@@ -1,6 +1,6 @@
 function newDivination() {
 	var book = Math.floor(Math.random() * 10);
 	var iteration = Math.floor(Math.random() * 60);
-	var divination = 'no magic for you yet' + book + iteration;
+	var divination = 'no magic for you yet {' + book + ',' + iteration + '}';
 	document.getElementById('divinationDisplay').innerHTML = divination;
 }
