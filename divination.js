70f76406440ff66c9159f0f3a2b06d353ef11ec2
seanjones2848 <seanjones2848@gmail.com>
2017-11-05 10:56:50 -0800

    starting loading magic

--- divination.js
+++ divination.js
@@ -1,6 +1,22 @@
+
+function loadMagic() {
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET", "http://github.com/seanjones2848/computer_musings/txt/book_chop/div_666");
+	rawFile.onreadystatechange = function() {
+		if (rawFile.readyState === 4) {
+			if (rawFile.status === 200 || rawFile.status == 0) {
+				var allMagic = rawFile.responseText;
+				alert(allMagic);
+				console.log(allMagic);
+			}
+		}
+	}
+}
+
 function newDivination() {
 	var book = Math.floor(Math.random() * 10);
 	var iteration = Math.floor(Math.random() * 60);
-	var divination = 'no magic for you yet {' + book + ',' + iteration + '}';
+	var divination = '<p>no magic for you yet {' + book + ',' + iteration + '}</p>';
+	loadMagic();
 	document.getElementById('divinationDisplay').innerHTML = divination;
 }
