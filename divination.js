2ecbf2f70fa2c46360202de8ac7b634f550f7cf6
seanjones2848 <seanjones2848@gmail.com>
2017-11-04 18:33:37 -0700

    doop

--- divination.js
+++ divination.js
@@ -1,5 +1,6 @@
 function newDivination() {
 	var book = Math.floor(Math.random() * 10);
 	var iteration = Math.floor(Math.random() * 60);
-	document.getElementById('divinationDisplay').innerHTML = 'no magic for you yet' + book + ieration;
+	var divination = 'no magic for you yet' + book + iteration
+	document.getElementById('divinationDisplay').innerHTML = divination;
 }
