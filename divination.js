c03edd0b2fead8e54213a09e74172d812c277de2
seanjones2848 <seanjones2848@gmail.com>
2018-07-12 22:29:40 -0700

    fixed it up

--- divination.js
+++ divination.js
@@ -1,15 +1,12 @@
 function newDivination() {
+	var book = Math.floor(Math.random() * 10);
+	var iteration = Math.floor(Math.random() * 59) + 1;
 	var raw_URL = "https://raw.githubusercontent.com/seanjones2848/computer_musings/master/txt/book_chop/div_"
 	var divs = [ "1", "24", "42", "5", "6", "666", "7", "777", "8", "9" ];
-	var book = Math.floor(Math.random() * 10);
 	var div_URL = raw_url + divs[book];
-	var div = loadStrings(div_URL, div_get);
-}
-
-function div_get(div) {
-	var iteration = Math.floor(Math.random() * 59) + 1;
-	var div_start = div.search("Iteration " + iteration);
-	var div_stop = div.search("Iteration " + (iteration + 1));
-	var divination = div.substring(div_start, div_stop);
+	var wholeBook = loadStrings(div_URL);
+	var div_start = wholeBook.search("Iteration " + iteration);
+	var div_stop = wholeBook.search("Iteration " + (iteration + 1));
+	var divination = "Book: " + book + " Chapter: " + iteration + " " + wholeBook.substring(div_start, div_stop);
 	document.getElementById('divinationDisplay').innerHTML = divination;
 }
