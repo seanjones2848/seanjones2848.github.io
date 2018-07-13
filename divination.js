d02cf8c40da60db2ea9bc93a6a09b2fa0ee34575
seanjones2848 <seanjones2848@gmail.com>
2018-07-12 23:55:10 -0700

    works, hopefully not ugly

--- divination.js
+++ divination.js
@@ -5,14 +5,14 @@ var divs = [ "1", "24", "42", "5", "6", "666", "7", "777", "8", "9" ];
 
 function setup() {
 	book = Math.floor(Math.random() * 10);
-	iteration = Math.floor(Math.random() * 59) + 1;
+	iteration = Math.floor(Math.random() * 58) + 1;
 	var div_URL = raw_URL + divs[book];
 	loadStrings(div_URL, get_div);
 }
 
 function get_div(Book) {
 	var wholeBook = JSON.stringify(Book);
-	var div_start = wholeBook.search(("Iteration " + iteration)) + 55;
+	var div_start = wholeBook.search(("Iteration " + iteration)) + 60;
 	var div_stop = wholeBook.search(("Iteration " + (iteration + 1)));
 	var divination = "Book: " + book + '","' + "Chapter: " + iteration + '","' + wholeBook.substring(div_start, div_stop);
 	var div = divination.replace(/\",\"/g, '<br>');
