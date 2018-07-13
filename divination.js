badfd0b64826aca46fe75a4227af42ea3847ff3c
seanjones2848 <seanjones2848@gmail.com>
2018-07-12 22:37:09 -0700

    spelling errors

--- divination.js
+++ divination.js
@@ -3,10 +3,11 @@ function newDivination() {
 	var iteration = Math.floor(Math.random() * 59) + 1;
 	var raw_URL = "https://raw.githubusercontent.com/seanjones2848/computer_musings/master/txt/book_chop/div_"
 	var divs = [ "1", "24", "42", "5", "6", "666", "7", "777", "8", "9" ];
-	var div_URL = raw_url + divs[book];
+	var div_URL = raw_URL + divs[book];
 	var wholeBook = loadStrings(div_URL);
 	var div_start = wholeBook.search("Iteration " + iteration);
 	var div_stop = wholeBook.search("Iteration " + (iteration + 1));
 	var divination = "Book: " + book + "\nChapter: " + iteration + "\n" + wholeBook.substring(div_start, div_stop);
+	console.log(divination);
 	document.getElementById('divinationDisplay').innerHTML = divination;
 }
